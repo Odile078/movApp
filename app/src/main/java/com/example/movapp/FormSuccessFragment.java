6724a453c200234b96f0e35c51d2d4812298a6c2
Odile078 <ouwimpuhwe620@daviscollege.com>
2020-12-20 11:39:10 +0200

    Adding the fragment logic

--- app/src/main/java/com/example/movapp/FormSuccessFragment.java
+++ app/src/main/java/com/example/movapp/FormSuccessFragment.java
@@ -1,4 +1,32 @@
 package com.example.movapp;
 
-public class FormSuccessFragment {
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.fragment.app.DialogFragment;
+
+public class FormSuccessFragment extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("SMS");
+        builder.setMessage("List successfully submitted");
+
+        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dismiss();
+            }
+        });
+        /*
+        builder.setNegativeButton("Nope", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dismiss();
+            }
+        });*/
+        return builder.create();
+    }
 }

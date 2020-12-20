1ae520e7a08af2c164e9e12eaf5f3c08e2d17acc
Odile078 <ouwimpuhwe620@daviscollege.com>
2020-12-20 11:52:34 +0200

    watchlist.java

--- app/src/main/java/com/example/movapp/WatchListActivity.java
+++ app/src/main/java/com/example/movapp/WatchListActivity.java
@@ -2,13 +2,32 @@ package com.example.movapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class WatchListActivity extends AppCompatActivity {
+    @BindView(R.id.movienameTextView)
+    TextView movienameTextView;
+    @BindView(R.id.moviecategoryTextView) TextView moviecategoryTextView;
+    @BindView(R.id.moviedetailTextView) TextView moviedetailTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_watch_list);
+
+        ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+        String movieName = intent.getStringExtra("movieName");
+        String movieCategory = intent.getStringExtra("movieCategory");
+        String movieDetail = intent.getStringExtra("movieDetail");
+        movienameTextView.setText("Here are all the movie: " + movieName);
+        moviecategoryTextView.setText("Here are all the Category: " + movieCategory);
+        moviedetailTextView.setText("Here are all the Detail: " + movieDetail);
     }
 }
